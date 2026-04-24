0bcc886b6b8e5a700afbb84f9e181a712628289e
Tlotliso215 <tlotlisosetebe215@gmail.com>
2026-04-24 16:28:43 +0200

    Add temperature tracking functionality

--- 4636286_setebe_Ex1.py
+++ 4636286_setebe_Ex1.py
@@ -1,18 +1,38 @@
-print("""
-FULL NAME: TLOTLISO SETEBE
-STUDENT NUMBER: 4636286 """)
+print("Temperature Tracker (type 'quit' to stop)")
 
-print("")
-print("EXERCISE 1")
-print("")
+temperature_list = []
 
-speed_above_limit = int(input("Enter km/h above the speed limit: "))
+temperature = input("Enter temperature or 'quit': ")
 
-if  0<speed_above_limit<10:
-    print("Warning")
-elif 11<speed_above_limit<20:
-    print("Fine: R500")
+while temperature != "quit":
+    temp = float(temperature)
+    temperature_list.append(temp)
+
+    temperature = input("Enter temperature or 'quit': ")
+
+# After loop (when user types quit)
+if len(temperature_list) > 0:
+    total = 0
+
+    for t in temperature_list:
+        total += t
+
+    average = total / len(temperature_list)
+
+    highest = temperature_list[0]
+    lowest = temperature_list[0]
+
+    for t in temperature_list:
+        if t > highest:
+            highest = t
+        if t < lowest:
+            lowest = t
+
+    print("\nResults:")
+    print("Temperatures:", temperature_list)
+    print("Highest:", highest)
+    print("Lowest:", lowest)
+    print("Average:", average)
+
 else:
-    print("Fine: R1000")
-
-
+    print("No temperatures entered.")            
